d4781d8eb26376bbee8e888cc15798f1e1792011
AndrewsSelvaraj <andrewsselvaraj@gmail.com>
2023-03-21 22:08:15 +0530

    Working version

--- src/main/java/com/cloud/examination/ExaminationApplication.java
+++ src/main/java/com/cloud/examination/ExaminationApplication.java
@@ -2,6 +2,8 @@ package com.cloud.examination;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ExaminationApplication {
